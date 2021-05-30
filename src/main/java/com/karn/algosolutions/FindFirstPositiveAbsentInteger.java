a3daa952c01830d5f5ba1456a5fc134378ff2ae9
Ashish Karn <ashishkrkarn@gmail.com>
2021-05-30 12:19:09 +0530

    Question description added

--- src/main/java/com/karn/algosolutions/FindFirstPositiveAbsentInteger.java
+++ src/main/java/com/karn/algosolutions/FindFirstPositiveAbsentInteger.java
@@ -1,5 +1,17 @@
 package com.karn.algosolutions;
 
+/**
+ * This problem was asked by Stripe.
+ * <p>
+ * Given an array of integers, find the first missing positive integer in
+ * linear time and constant space. In other words, find the lowest positive
+ * integer that does not exist in the array. The array can contain duplicates
+ * and negative numbers as well.
+ * <p>
+ * For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
+ * <p>
+ * You can modify the input array in-place.
+ */
 public class FindFirstPositiveAbsentInteger {
 
 
