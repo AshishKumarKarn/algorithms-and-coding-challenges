2167a2b9f09b7d6cbdc4af42f4b7b3f5337230ac
Ashish Karn <ashishkrkarn@gmail.com>
2021-09-14 18:05:56 +0530

    New solution LetterCombinationsOfAPhoneNumber

--- src/main/java/com/karn/leetcode/LetterCombinationsOfAPhoneNumber.java
+++ src/main/java/com/karn/leetcode/LetterCombinationsOfAPhoneNumber.java
@@ -11,37 +11,39 @@ public class LetterCombinationsOfAPhoneNumber {
         System.out.println(new LetterCombinationsOfAPhoneNumber().letterCombinations("23"));
     }
 
-    private static Map<Integer,String> numPad = new HashMap<>();
-    static{
-        numPad.put(2,"abc");
-        numPad.put(3,"def");
-        numPad.put(4,"ghi");
-        numPad.put(5,"jkl");
-        numPad.put(6,"mno");
-        numPad.put(7,"pqrs");
-        numPad.put(8,"tuv");
-        numPad.put(9,"wxyz");
+    private static Map<Integer, String> numPad = new HashMap<>();
+
+    static {
+        numPad.put(2, "abc");
+        numPad.put(3, "def");
+        numPad.put(4, "ghi");
+        numPad.put(5, "jkl");
+        numPad.put(6, "mno");
+        numPad.put(7, "pqrs");
+        numPad.put(8, "tuv");
+        numPad.put(9, "wxyz");
     }
+
     public List<String> letterCombinations(String digits) {
-        List<String> stringList=new ArrayList<>();
+        List<String> stringList = new ArrayList<>();
         //base condition
-        if(digits==null||digits.trim().length()==0) return stringList;
+        if (digits == null || digits.trim().length() == 0) return stringList;
 
         char[] chars = digits.toCharArray();
         for (char c : chars) {
             stringList.add(numPad.get(c - 48));
         }
 
-        List<String> result=new ArrayList<>();
+        List<String> result = new ArrayList<>();
         int length = digits.length();
         //abc-> def=> ad bd cd ae be de af bf cf===9
         //abc-> def-> ghi => adg adh adi bdg bdh bdi cdg cdh cdi aeg aeh aei beg
         //beh bei ceg ceh cei afg afh afi bfg bfh bfi cfg cfh cfi===27
-        if(length==4){
-            String[] str1=stringList.get(0).split("");
-            String[] str2=stringList.get(1).split("");
-            String[] str3=stringList.get(2).split("");
-            String[] str4=stringList.get(3).split("");
+        if (length == 4) {
+            String[] str1 = stringList.get(0).split("");
+            String[] str2 = stringList.get(1).split("");
+            String[] str3 = stringList.get(2).split("");
+            String[] str4 = stringList.get(3).split("");
             for (String element : str1) {
                 for (String item : str2) {
                     for (String value : str3) {
@@ -52,10 +54,10 @@ public class LetterCombinationsOfAPhoneNumber {
                 }
             }
 
-        }else if(length==3){
-            String[] str1=stringList.get(0).split("");
-            String[] str2=stringList.get(1).split("");
-            String[] str3=stringList.get(2).split("");
+        } else if (length == 3) {
+            String[] str1 = stringList.get(0).split("");
+            String[] str2 = stringList.get(1).split("");
+            String[] str3 = stringList.get(2).split("");
             for (String s : str1) {
                 for (String value : str2) {
                     for (String item : str3) {
@@ -64,17 +66,17 @@ public class LetterCombinationsOfAPhoneNumber {
                 }
             }
 
-        }else if(length==2){
-            String[] str1=stringList.get(0).split("");
-            String[] str2=stringList.get(1).split("");
+        } else if (length == 2) {
+            String[] str1 = stringList.get(0).split("");
+            String[] str2 = stringList.get(1).split("");
 
             for (String s : str1) {
                 for (String value : str2) {
                     result.add(s + value);
                 }
             }
-        }else if(length==1){
-            String str=stringList.get(0);
+        } else if (length == 1) {
+            String str = stringList.get(0);
             result = Arrays.asList(str.split("").clone());
         }
         return result;
