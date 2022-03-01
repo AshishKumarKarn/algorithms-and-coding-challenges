d0c0722b614263454199aa66e2b69d8816fa5a7a
ashishkarn <ashishkrkarn@gmail.com>
2022-03-01 23:53:24 +0530

    new implementations

---
+++ src/main/java/com/karn/leetcode/SubarraySumEqualsK.java
@@ -0,0 +1,34 @@
+package com.karn.leetcode;
+
+import java.util.*;
+
+public class SubarraySumEqualsK {
+    public static void main(String[] args) {
+        int[] nums = {1, -1, 0};
+        //[1,3,4,5,-1,5]
+        //4
+
+        //-1,0,1
+        //[1,-1,0] [0] [1,-1]
+        System.out.println(new SubarraySumEqualsK().subarraySum(nums, 0));
+    }
+
+    public int subarraySum(int[] nums, int k) {
+        int result = 0;
+        int sum = 0;
+        Map<Integer, Integer> memory = new HashMap<>();
+        memory.put(0, 1);
+        for (int num : nums) {
+            sum += num;
+            //don't add element before querying for current required value
+            if (memory.get((sum - k)) != null) {
+                result += memory.get((sum - k));
+            }
+            Integer lastValue = memory.put(sum, 1);
+            if (lastValue != null) {
+                memory.put(sum, (1 + lastValue));
+            }
+        }
+        return result;
+    }
+}
