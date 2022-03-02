7559e1abf28e5fbe07fea00afa4479c309ed420c
ashishkarn <ashishkrkarn@gmail.com>
2022-03-02 22:49:49 +0530

    new implementations

--- src/main/java/com/karn/leetcode/TopKFrequentElements.java
+++ src/main/java/com/karn/leetcode/TopKFrequentElements.java
@@ -6,17 +6,17 @@ public class TopKFrequentElements {
     public int[] topKFrequent(int[] nums, int k) {
         int[] result = new int[k];
         Map<Integer, Integer> memory = new HashMap<>();
-        Set<Map.Entry<Integer,Integer>> queue = new TreeSet<>((a,b)->a.getValue()>=b.getValue()?-1:1);
+        Set<Map.Entry<Integer, Integer>> queue = new TreeSet<>((a, b) -> a.getValue() >= b.getValue() ? -1 : 1);
         for (int num : nums) {
             Integer value = memory.getOrDefault(num, 0);
-            memory.put(num,value+1);
+            memory.put(num, value + 1);
         }
 
         queue.addAll(memory.entrySet());
         // System.out.println(queue);
-        int i=0;
-        for (Map.Entry<Integer,Integer> entry:queue) {
-            if(k==0){
+        int i = 0;
+        for (Map.Entry<Integer, Integer> entry : queue) {
+            if (k == 0) {
                 break;
             }
             result[i] = entry.getKey();
