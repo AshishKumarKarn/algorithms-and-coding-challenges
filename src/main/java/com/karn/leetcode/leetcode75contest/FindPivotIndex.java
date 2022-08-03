ae4c49c72570733c493b50f8a66535b8d2c2bdb2
Ashish Karn <ashishkrkarn@gmail.com>
2022-08-04 00:52:46 +0530

    New solution

---
+++ src/main/java/com/karn/leetcode/leetcode75contest/FindPivotIndex.java
@@ -0,0 +1,23 @@
+package com.karn.leetcode.leetcode75contest;
+
+import java.util.Arrays;
+
+public class FindPivotIndex {
+    public int pivotIndex(int[] nums) {
+        int sum=0;
+        int left=0;
+        for (int num : nums) {
+            sum += num;
+        }
+        int right = sum;
+        System.out.println(sum);
+        for (int i = 0; i < nums.length; i++) {
+            if(left==right-nums[i]){
+                return i;
+            }
+            left += nums[i];
+            right -= nums[i];
+        }
+        return -1;
+    }
+}
