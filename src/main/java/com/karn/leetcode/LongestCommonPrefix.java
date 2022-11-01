65dc8974df7d4050fc0340f7828e224284ac9d4f
Ashish Karn <ashishkrkarn@gmail.com>
2022-11-01 08:57:43 +0530

    new implementations

--- src/main/java/com/karn/leetcode/LongestCommonPrefix.java
+++ src/main/java/com/karn/leetcode/LongestCommonPrefix.java
@@ -2,27 +2,27 @@ package com.karn.leetcode;
 
 public class LongestCommonPrefix {
     public String longestCommonPrefix(String[] strs) {
-        if(strs.length<1){
+        if (strs.length < 1) {
             return "";
         }
         String first = strs[0];
         String second;
-        for(int i=1;i<strs.length;i++){
-            boolean flipped=false;
-            if(first.length()>strs[i].length()){
+        for (int i = 1; i < strs.length; i++) {
+            boolean flipped = false;
+            if (first.length() > strs[i].length()) {
                 second = first;
-                first=strs[i];
-                flipped=true;
-            }else{
+                first = strs[i];
+                flipped = true;
+            } else {
                 second = strs[i];
             }
-            for(int j=0;j<first.length()&&j<second.length();j++){
-                if(first.charAt(j)!=second.charAt(j)&&j>0){
-                    if(flipped)
-                        first=first.substring(0,j);
+            for (int j = 0; j < first.length() && j < second.length(); j++) {
+                if (first.charAt(j) != second.charAt(j) && j > 0) {
+                    if (flipped)
+                        first = first.substring(0, j);
                     else
-                        first=second.substring(0,j);
-                }else if(first.charAt(j)!=second.charAt(j)&&j==0){
+                        first = second.substring(0, j);
+                } else if (first.charAt(j) != second.charAt(j) && j == 0) {
                     return "";
                 }
             }
