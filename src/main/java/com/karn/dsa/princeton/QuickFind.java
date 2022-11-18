1410b3074a6d1a902ae5223b67859aa771c04fb0
Ashish Karn <ashishkrkarn@gmail.com>
2022-11-18 23:26:59 +0530

    new implementations

--- src/main/java/com/karn/dsa/princeton/QuickFind.java
+++ src/main/java/com/karn/dsa/princeton/QuickFind.java
@@ -4,16 +4,24 @@ import java.util.Arrays;
 
 public class QuickFind {
     public static void main(String[] args) {
-        QuickFind quickFind = new QuickFind(9);
-        quickFind.union(5, 4);
+        QuickFind quickFind = new QuickFind(10);
+        quickFind.union(0, 6);
         quickFind.union(2, 6);
-        quickFind.union(7, 6);
-        quickFind.union(3, 8);
+        quickFind.union(2, 7);
+        quickFind.union(3, 4);
+        quickFind.union(4, 5);
+        quickFind.union(4, 8);
+        quickFind.union(1, 8);
+
         System.out.println(Arrays.toString(quickFind.ids));
-        boolean sol1 = quickFind.connected(0, 6);
-        System.out.println(sol1);//false
-        boolean sol2 = quickFind.connected(2, 7);
+        boolean sol1 = quickFind.connected(3, 1);//true
+        System.out.println(sol1);
+        boolean sol2 = quickFind.connected(2, 7);//true
         System.out.println(sol2);//true
+        System.out.println(quickFind.connected(7,3));//false
+        System.out.println(quickFind.connected(0,7));//true
+        System.out.println(quickFind.connected(1,9));//false
+
     }
         private int[] ids;
     QuickFind(int n){
